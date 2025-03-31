3532c7b6b0d145eb783b64191864b177064234ae
DiegoGieminiani <diegoalonso.gieminiani@gmail.com>
2025-03-31 15:59:58 -0300

    Adición de @login_required

--- tasks/views.py
+++ tasks/views.py
@@ -7,6 +7,7 @@ from django.db import IntegrityError
 from .forms import TaskForm
 from .models import Task
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -40,11 +41,17 @@ def signup(request):
             'error': 'Password do not match'
         })
 
+@login_required
 def tasks(request):
     tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
-
     return render(request, 'tasks.html', {'tasks': tasks})
 
+@login_required
+def tasks_completed(request):
+    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
+    return render(request, 'tasks.html', {'tasks': tasks})
+
+@login_required
 def create_task(request):    
     if request.method == 'GET':  
         return render(request, 'create_task.html',{
@@ -62,7 +69,8 @@ def create_task(request):
             'form': TaskForm,
             'error': 'Please provide valid data'
         })
-
+ 
+@login_required
 def task_detail(request, task_id):
     if request.method == 'GET':
         task = get_object_or_404(Task,pk=task_id, user=request.user)
@@ -81,6 +89,7 @@ def task_detail(request, task_id):
             'error': "Error updating task"
         })
 
+@login_required
 def complete_task(request, task_id):
     task = get_object_or_404(Task, pk=task_id, user=request.user)
     if request.method == 'POST':
@@ -88,12 +97,14 @@ def complete_task(request, task_id):
         task.save()
         return redirect('tasks')
 
+@login_required
 def delete_task(request, task_id):
     task = get_object_or_404(Task, pk=task_id, user=request.user)
     if request.method == 'POST':
         task.delete()
         return redirect('tasks')
 
+@login_required
 def signout (request):   
     logout(request)
     return redirect('home')
